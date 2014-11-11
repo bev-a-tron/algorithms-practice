d0edc907510c66aa87bc0bb468c5d216f8f75789
bev-a-tron <beverly.a.lau@gmail.com>
2014-11-10 23:23:52 -0800

    [BL] Refactor

--- stockprices.py
+++ stockprices.py
@@ -1,37 +1,19 @@
 # stock prices from interviewcake
 
-# Writing interview questions hasn't made me rich. Maybe trading Apple stocks will.
-# I have an array stockPricesYesterday where:
-# The indices are the time, as a number of minutes past trade opening time, which was 9:30am local time.
-# The values are the price of Apple stock at that time, in dollars.
-# For example, the stock cost $500 at 10:30am, so stockPricesYesterday[60] = 500.
-# Write an efficient algorithm for computing the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday. For this problem, we won't allow "shorting"—you must buy before you sell.
+# stockprices[i] is an nx1 array with one company's stock price information for each minute of the day.
+# write an algorithm to maximize the profit from one buy, one sell.
+# no shorting allowed.
 
 def get_max_profit(stockprices):
 	max_profit = 0
 	for i in range(len(stockprices)):
-		lowest = low(stockprices, 0, i)
-		highest = high(stockprices, i, len(stockprices))
+		lowest = min(stockprices[0:i+1])
+		highest = max(stockprices[i:len(stockprices)+1])
 		this_profit = highest - lowest
 		if this_profit > max_profit:
 			max_profit = this_profit
 	return max_profit
 
-def low(stockprices, left, right):
-	lowest_price = stockprices[0]
-	for i in range(left, right):
-		if stockprices[i] < lowest_price:
-			lowest_price = stockprices[i]
-	return lowest_price
-
-def high(stockprices, left, right):
-	highest_price = stockprices[-1]
-	for i in range(left, right):
-		if stockprices[i] > highest_price:
-			highest_price = stockprices[i]
-	return highest_price
-
-
 if __name__ == "__main__":
-	ans = get_max_profit([30,10,100,20,500])
+	ans = get_max_profit([30,10,100,5,20,500,900,10,20,1])
 	print "The max profit is: $%u"%(ans)
